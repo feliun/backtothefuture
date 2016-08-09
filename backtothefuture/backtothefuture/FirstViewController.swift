b149725318f885388ad2f4ac9877ad08c4d32402
feliun <felipe.polo@gmail.com>
2016-08-09 19:19:04 +0100

    Creating initial layout for first view

--- backtothefuture/backtothefuture/FirstViewController.swift
+++ backtothefuture/backtothefuture/FirstViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class FirstViewController: UIViewController {
 
+    @IBOutlet weak var label1: UILabel!
+    @IBOutlet weak var label2: UILabel!
+    @IBOutlet weak var label3: UILabel!
+    @IBOutlet weak var label4: UILabel!
+    @IBOutlet weak var timeLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
