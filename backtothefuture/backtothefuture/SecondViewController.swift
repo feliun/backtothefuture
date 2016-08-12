66d95f5fc8164670041a4a80506795d844cc24b7
feliun <felipe.polo@gmail.com>
2016-08-12 23:08:48 +0100

    Layout for the second view

--- backtothefuture/backtothefuture/SecondViewController.swift
+++ backtothefuture/backtothefuture/SecondViewController.swift
@@ -10,6 +10,15 @@ import UIKit
 
 class SecondViewController: UIViewController {
 
+    @IBOutlet weak var label1: UILabel!
+    @IBOutlet weak var label2: UILabel!
+    @IBOutlet weak var label3: UILabel!
+    @IBOutlet weak var label4: UILabel!
+    
+    @IBAction func ButtonClicked(_ sender: UIButton) {
+    
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
